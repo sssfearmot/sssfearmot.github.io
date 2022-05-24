1e6f2a5fc959ad3eaa0743752d5321d67229f33d
sssfearmot <71980176+sssfearmot@users.noreply.github.com>
2022-05-24 18:18:28 +0900

    Add files via upload

---
+++ trn01/trn01/js/main.js
@@ -0,0 +1,9 @@
+$(function(){ /*html 링크 상단에 쓸때 적용*/
+    $('.main_slider').slick({
+        arrows: false,
+        autoplay: true,
+        pauseOnHover: false, /* 마우스를 올려도 작동 */
+        pauseOnFocus: false,
+    })
+})
+
