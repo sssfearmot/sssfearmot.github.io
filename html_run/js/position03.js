d0e6b9d05b876df5468ba5d5a198ee23ae55e35d
sssfearmot <71980176+sssfearmot@users.noreply.github.com>
2022-06-03 17:34:57 +0900

    Add files via upload

---
+++ html_run/js/position03.js
@@ -0,0 +1,8 @@
+$(function () {
+    $('header i').on('click', function () {
+        $('.cover').toggleClass('on')
+    })
+    $('.cover a').on('click', function () {
+        $('.cover').toggleClass('on')
+    })
+})
