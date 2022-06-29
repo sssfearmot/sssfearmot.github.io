bdd9df803fd39ffa6ef247e2acd4c58c591733a6
IMMJ <story_mania@naver.com>
2022-06-29 17:53:44 +0900

    2022-06-29

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,5 +1,15 @@
-$('.swiper_wrapper').slick({
-    dots: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
+$(function(){
+
+    $('.swiper_wrapper').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+    });
+
+    $('.full_slider').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 1,
+    })
 })
