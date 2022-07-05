fb6f3a7c6745d386e2bb88f592a5f07f1f5a6efa
IMMJ <story_mania@naver.com>
2022-07-05 17:53:06 +0900

    2022-07-25

--- portfolio/02/js/index.js
+++ portfolio/02/js/index.js
@@ -1,7 +1,18 @@
-$(function(){
+$(function () {
     $('.slider').slick({
         dots: true,
         arrows: false,
         slidesToShow: 3,
+    })
+
+    $('.pb_tabpanel_body').slick({
+        centerMode: true,
+        arrows: true,
+    })
+
+    $('.pb_tablist>li .tab').on('click', function (e) {
+        e.preventDefault();
+        $('.pb_tablist>li').removeClass('on');
+        $(this).parent().addClass('on');
     })
 })
