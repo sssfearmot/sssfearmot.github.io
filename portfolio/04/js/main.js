9caec35dc0a1135f2b253745ec82de978d88039f
IMMJ <story_mania@naver.com>
2022-08-02 15:03:23 +0900

    2022-08-02

--- portfolio/04/js/main.js
+++ portfolio/04/js/main.js
@@ -35,7 +35,7 @@ $(function () {
     // 
 
     function scrollUp() {
-        const scrollUp = document.getElementById('scroll-up');
+        const scrollUp = document.querySelector('.scrollup');
         if (this.scrollY >= 200) scrollUp.classList.add('show-scroll');
         else scrollUp.classList.remove('show-scroll')
     }
