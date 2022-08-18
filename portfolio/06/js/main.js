5747a2213c3707b8f40cd94c94269bd18b9278b3
IMMJ <story_mania@naver.com>
2022-08-18 17:52:20 +0900

    2022-08-18

--- portfolio/06/js/main.js
+++ portfolio/06/js/main.js
@@ -115,7 +115,7 @@ const names = [
 ];
 
 const descriptions = [
-    ["Bring a piece of history to the city’s urban streets as you walk into Nike Jordan Max Aura 3 men’s sneakers. Inspired by the rich Jordanian heritage, this model has the energy of basketball shoes and a look that changes the perception of the classic style."],
+    ["나이키 조던 맥스 아우라 3 남성 운동화를 신고 걸으면서 도시의 도시 거리로 역사의 한 조각을 가져오세요. 풍부한 요르단 유산에 영감을 받은 이 모델은 농구화의 에너지와 클래식 스타일에 대한 인식을 바꾸는 룩을 가지고 있습니다."],
     ["Nike Air Max 95 men's sneakers move you with the strength and fluidity inspired by the anatomy of the human body. The central sole forms the basis of these sneakers, while the structured side panels give a solid and stable construction. Flexible incisions in the sole allow your feet to move naturally."],
     ["Jordan Delta 2 SP men's basketball shoes offer a fresh and fearless approach to the characteristics you want: durability, comfort and the attitude of the Jordan brand. The first model of Delta 2 sneakers, with the same idea, received redesigned lines and modified components."]
 ];
@@ -146,18 +146,15 @@ function animate(element, time, anim) {
     }, time);
 }
 
-//Assign colors to color buttons
 function assignColors(i, shoe) {
     colorBtn[i].style.background = colors[shoe - 1][i];
 }
 
-//Set rating by filling out stars
 function resetStars(shoe) {
     for (let i = 0; i < stars.length; i++) {
         stars[i].innerText = "star_outline";
     }
 
-    //Adding the ratings
     for (let i = 0; i < ratings[shoe]; i++) {
         stars[i].innerText = "star";
     }
@@ -171,8 +168,6 @@ for (let i = 0; i < sizes.length; i++) {
     });
 }
 
-/*Setting up all of the initial data 
-for the first shoe that loads*/
 shoeTotal.innerText = "0" + pag.length;
 shoeNum.innerText = "0" + shoe;
 price.innerText = "$" + prices[0];
@@ -183,47 +178,35 @@ desc.innerText = descriptions[0];
 //Changing Images
 for (let i = 0; i < thumb.length; i++) {
     thumb[i].addEventListener('click', (e) => {
-        /*JS arrays start with 0, 
-        so we set the id to i + 1*/
         id = i + 1;
 
-        /*Setting the main image to clicked thumbnail image*/
         img.src = getImage("showcase", shoe, colorType, id, 'png');
 
-        //Adding the active class to the clicked thumbnail
         resetActive(thumb, 'thumb', i);
 
-        //Adding the fade in animation on the shoe
         animate(imgContainer, 550, "fade 500ms ease-in-out");
     });
 }
 
 for (let i = 0; i < colorBtn.length; i++) {
-    //Setting up colors to the color btn
     assignColors(i, shoe);
 
     //Changing Colors
     colorBtn[i].addEventListener('click', () => {
-        //Change color type of the shoe
         colorType = i + 1;
 
-        //Change Showcase Image
         setTimeout(() => {
             img.src = getImage("showcase", shoe, colorType, id, 'png');
         }, 450);
 
-        //Change Thumbnails
         for (let i = 0; i < thumb.length; i++) {
             thumb[i].src = getImage("thumbs", shoe, colorType, i + 1, 'jpg');
         }
 
-        //Set active class to clicked button
         resetActive(colorBtn, 'color', i);
 
-        //Change the shoe title
         title.innerText = names[shoe - 1][i];
 
-        //Adding all of the animations
         animate(img, 550, "jump 500ms ease-in-out");
         animate(shadow, 550, "shadow 500ms ease-in-out");
     });
@@ -232,68 +215,51 @@ for (let i = 0; i < colorBtn.length; i++) {
 
 /*===== Slider =====*/
 function slider(shoe) {
-    //Change Showcase Image
     setTimeout(() => {
         img.src = getImage("showcase", shoe, colorType, id, 'png');
     }, 600);
 
-    //Change Thumbnails 
     for (let i = 0; i < thumb.length; i++) {
         thumb[i].src = getImage("thumbs", shoe, colorType, i + 1, 'jpg');
     }
 
-    //Changing the colors on the color buttons
     for (let i = 0; i < colorBtn.length; i++) {
         assignColors(i, shoe);
     }
 
-    //Set active class to clicked button
     resetActive(pag, 'pag', shoe - 1);
 
-    //Reassign all of the shoe data 
     desc.innerText = descriptions[shoe - 1];
     title.innerText = names[shoe - 1][colorType - 1];
     price.innerText = "$" + prices[shoe - 1];
     resetStars(shoe - 1);
     shoeNum.innerText = "0" + shoe;
 
-    //Adding all of the animations
     animate(img, 1550, "replace 1.5s ease-in");
     animate(shadow, 1550, "shadow2 1.5s ease-in");
 }
 
 //Previous Shoe
 prev.addEventListener('click', () => {
-    //Decrement img id
     shoe--;
-    /*Check if slide goes below the first, 
-    and reset it to the last slide*/
     if (shoe < 1) {
         shoe = pag.length;
     }
-    //Run the slider function
     slider(shoe);
 });
 
 next.addEventListener('click', () => {
-    //Increment img id
     shoe++;
-    /*Check if slider goes above the slides length, 
-    and reset it to the first*/
     if (shoe > pag.length) {
         shoe = 1;
     }
-    //Run the slider function
     slider(shoe);
 });
 
 //Pagination
 for (let i = 0; i < pag.length; i++) {
-    //Add click event for all pagination
     pag[i].addEventListener('click', () => {
-        //Run the slider function
         slider(i + 1);
-        //Set shoe id to clicked pagination index
         shoe = i + 1;
     });
 }
@@ -314,3 +280,24 @@ for (let i = 0; i < sizes.length; i++) {
         sizes[i].classList.toggle('active');
     });
 }
+
+// header
+const menu = document.querySelector(".menu");
+const menuBtn = document.querySelector(".menu_btn");
+
+menuBtn.addEventListener("click", () => {
+    menu.classList.toggle('nav-toggle');
+});
+
+// play video
+$(document).ready(function () {
+    $('.popup-youtube').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+
+        fixedContentPos: false
+    });
+});
