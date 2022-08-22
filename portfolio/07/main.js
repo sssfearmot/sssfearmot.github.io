7e45ad978517c4a830bdd34e10224c29421b62be
IMMJ <story_mania@naver.com>
2022-08-22 17:51:35 +0900

    2022-08-22

--- portfolio/07/main.js
+++
@@ -1,8 +0,0 @@
-//Get elements from the DOM
-const menu = document.querySelector('.menu');
-const menuBtn = document.querySelector('.menu-btn');
-
-//Toggle open/close menu 
-menuBtn.addEventListener('click', () => {
-  menu.classList.toggle('menu-open');
-});
