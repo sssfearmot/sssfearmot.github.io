b1d9cd4fb6dec77df1986662175e93902bafd4df
IMMJ <story_mania@naver.com>
2022-08-31 17:56:25 +0900

    2022-08-31

--- portfolio/08/js/main.js
+++ portfolio/08/js/main.js
@@ -247,3 +247,102 @@ function scrollHeader() {
     else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+// the characters
+const items = document.querySelectorAll('.item'),
+    controls = document.querySelectorAll('.control'),
+    headerItems = document.querySelectorAll('.item-header'),
+    descriptionItems = document.querySelectorAll('.item-description'),
+    activeDelay = .76,
+    interval = 5000;
+
+let current = 0;
+
+const slider = {
+    init: () => {
+        controls.forEach(control => control.addEventListener('click', (e) => { slider.clickedControl(e) }));
+        controls[current].classList.add('active');
+        items[current].classList.add('active');
+    },
+    nextSlide: () => {
+        slider.reset();
+        if (current === items.length - 1) current = -1;
+        current++;
+        controls[current].classList.add('active');
+        items[current].classList.add('active');
+        slider.transitionDelay(headerItems);
+        slider.transitionDelay(descriptionItems);
+    },
+    clickedControl: (e) => {
+        slider.reset();
+        clearInterval(intervalF);
+
+        const control = e.target,
+            dataIndex = Number(control.dataset.index);
+
+        control.classList.add('active');
+        items.forEach((item, index) => {
+            if (index === dataIndex) {
+                item.classList.add('active');
+            }
+        })
+        current = dataIndex;
+        slider.transitionDelay(headerItems);
+        slider.transitionDelay(descriptionItems);
+        intervalF = setInterval(slider.nextSlide, interval);
+    },
+    reset: () => {
+        items.forEach(item => item.classList.remove('active'));
+        controls.forEach(control => control.classList.remove('active'));
+    },
+    transitionDelay: (items) => {
+        let seconds;
+
+        items.forEach(item => {
+            const children = item.childNodes;
+            let count = 1,
+                delay;
+
+            item.classList.value === 'item-header' ? seconds = .015 : seconds = .007;
+
+            children.forEach(child => {
+                if (child.classList) {
+                    item.parentNode.classList.contains('active') ? delay = count * seconds + activeDelay : delay = count * seconds;
+                    child.firstElementChild.style.transitionDelay = `${delay}s`;
+                    count++;
+                }
+            })
+        })
+    },
+}
+
+let intervalF = setInterval(slider.nextSlide, interval);
+slider.init();
+
+// Video Popup
+const video = document.querySelectorAll('.video');
+const button = document.querySelector('.video-control');
+const videoWrapper = document.querySelector('.video-wrapper');
+
+console.log([items])
+['.watch-button', '.actor-video'].forEach((el) => {
+    document.querySelector(el).onclick = () => {
+        videoWrapper.classList.add('active');
+    };
+});
+
+document.querySelector('.close-video').onclick = () => {
+    videoWrapper.classList.remove('active');
+};
+
+function playpausevideo() {
+    if (video.paused) {
+        button.innerHTML = "<i class='bx bx-pause' ></i>";
+        video.play();
+    } else {
+        button.innerHTML = "<i class='bx bx-play' ></i>";
+        video.pause();
+    }
+}
+
+button.addEventListener('click', playpausevideo);
